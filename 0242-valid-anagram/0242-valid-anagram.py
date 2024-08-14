2c0f5227e48d8511e0894fcdc0a4dac06660dede
umangnaith <152839736+umangnaith@users.noreply.github.com>
2024-08-14 16:34:47 +0530

    Time: 40 ms (91.59%), Space: 16.9 MB (85.25%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        a_dist,b_dist=list(set(s)),list(set(t))
+        a_dic_count,b_dic_count={},{}
+        for i in a_dist:
+            a_dic_count[i] = s.count(i)
+        for j in b_dist:
+            b_dic_count[j] = t.count(j)
+        if a_dic_count == b_dic_count:
+            return True
+        else:
+            return False
+
