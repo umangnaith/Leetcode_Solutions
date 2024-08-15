fd61ca1f17f0bcd4dbe6cd5719011d47f00a7078
umangnaith <152839736+umangnaith@users.noreply.github.com>
2024-08-15 12:25:33 +0530

    Time: 516 ms (70.17%), Space: 31.2 MB (12.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,6 +1,7 @@
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        sums=0
         current_sum = 0
         max_sum = nums[0]
         
@@ -11,3 +12,6 @@ class Solution:
             max_sum = max(max_sum, current_sum)
         
         return max_sum
+
+
+        
